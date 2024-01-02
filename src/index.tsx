d0a55fdeb9daefc1703c1656d1b81923ba824823
curocami <berettaxx@gmail.com>
2024-01-02 16:30:24 +0900

    something was fixed .

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+    import React from 'react'
+    import ReactDOM from 'react-dom/client'
+    import './index.css'
+    import App from './App'
+    import reportWebVitals from './reportWebVitals'
+    import { QueryClient, QueryClientProvider } from 'react-query'
+    import { ReactQueryDevtools } from 'react-query/devtools'
+    import { store } from './app/store'
+    import { Provider } from 'react-redux'
+     
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+          refetchOnWindowFocus: false,
+        },
+      },
+    })
+     
+    const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+    root.render(
+      // <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      // </React.StrictMode>
+    )
+     
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals()
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  
